75812e406364acf3e071f0a7c82415e3d3421460
efom444 <efortiz@unimayor.edu.co>
2019-04-14 14:54:51 -0500

    Revert "Update MainActivity.java"
    This reverts commit cd0e088161f3a457eb06b9da95f10e98d10623bd.

--- app/src/main/java/com/efom/popashopmovil/MainActivity.java
+++ app/src/main/java/com/efom/popashopmovil/MainActivity.java
@@ -1,14 +1,20 @@
 package com.efom.popashopmovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Login login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //codigo
+     login= new Login();
+
+     //login.prueba();
+
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
     }
 }
